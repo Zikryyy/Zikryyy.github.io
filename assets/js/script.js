814f4fd6beda3e6f1166beb37b87f12a79278f27
Zikryyy <zkry098zkry@gmail.com>
2025-04-14 16:13:45 +0800

    initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Update copyright year automatically
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
